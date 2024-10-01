397bd861502fcaa996f6d3b8cae3a46c4f36dd1d
Santo2011 <125989475+Santo2011@users.noreply.github.com>
2024-10-01 05:58:48 +0530

    app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if st.session_state.step == 1:
     # Move to the next step after 3 seconds
     time.sleep(3)
     st.session_state.step = 2
-    st.experimental_rerun()
+    st.rerun()
 
 # Step 2: Display the punishment message and first pleading title
 if st.session_state.step == 2:
@@ -52,14 +52,14 @@ if st.session_state.step == 2:
     if col1.button("No"):
         if st.session_state.pleading_level < len(pleading_titles) - 1:
             st.session_state.pleading_level += 1  # Move to the next pleading title
-            st.experimental_rerun()  # Reload the page to show the next title
+            st.rerun()  # Reload the page to show the next title
         else:
             st.markdown('<p style="font-size: 32px; font-weight: bold; color: #F8D3B7;">Oh no! You’re breaking my heart! Please, dear, say yes!</p>', unsafe_allow_html=True)
 
     # Handle "Yes" button click
     if col2.button("Yes"):
         st.session_state.step = 3  # Move to the next step
-        st.experimental_rerun()  # Reload the page to show the next message
+        st.rerun()  # Reload the page to show the next message
 
 # Step 3: Capture image after user agrees
 if st.session_state.step == 3:
@@ -76,7 +76,7 @@ if st.session_state.step == 3:
         st.markdown(f"<h2 style='color: #FFB3BA;'>Picture Capturing in {st.session_state.countdown} seconds!</h2>", unsafe_allow_html=True)
         time.sleep(1)  # Wait for a second
         st.session_state.countdown -= 1  # Decrease the countdown
-        st.experimental_rerun()  # Rerun to update the countdown display
+        st.rerun()  # Rerun to update the countdown display
     else:
         # Time's up, take the picture
         img = st.camera_input("Capture your beautiful smile")
@@ -87,7 +87,7 @@ if st.session_state.step == 3:
             st.session_state.step = 4  # Move to the next step
             st.session_state.countdown_active = False  # Reset countdown state
             st.session_state.countdown = 3  # Reset countdown for next use
-            st.experimental_rerun()
+            st.rerun()
 
 # Step 4: Display the download link and final punishment message
 if st.session_state.step == 4 and st.session_state.image_captured:
